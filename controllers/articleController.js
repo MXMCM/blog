dad1b0fa5629fa365a25d0a8766eb8c806188de5
n57r <n57r@fresnou.org>
2022-03-17 08:57:08 -0700

    second

---
+++ controllers/articleController.js
@@ -0,0 +1,30 @@
+const {Article, Comment, Reply} = require('../models');
+
+module.exports.renderAddForm = function(req, res){
+    const article = {
+        title: '',
+        intro: '',
+        image_url: '',
+        body: '',
+    };
+    res.render('articles/add', {article});
+};
+
+module.exports.addArticle = async function(req, res){
+    await Article.create({
+        title: req.body.title,
+        intro: req.body.intro,
+        image_url: req.body.image_url,
+        body: req.body.body,
+        author_id: 1,
+        published_on: new Date()
+    })
+    res.redirect('/');
+}
+
+module.exports.displayArticle = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId, {
+        include: ['author']
+    });
+    res.render('articles/view', {article});
+};
